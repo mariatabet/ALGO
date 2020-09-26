5e4f7165f14f84d73607ba09dc3b5a58e157da15
TABET DJALAL <dtabet@oppido.fr>
2020-09-26 20:39:32 +0200

    commit initial

---
+++ src/main/resources/ex1.js
@@ -0,0 +1,17 @@
+var a = 1;
+var b = 5;
+
+console.log("BEFORE");
+console.log("A: "+ a );
+console.log("B: "+ b );
+
+var c = a;
+a = b;
+b = c ;
+
+console.log("AFTER");
+console.log("A: "+ a );
+console.log("B: "+ b );
+
+
+
---
+++ src/main/resources/ex2.js
@@ -0,0 +1,12 @@
+var nom = "maria";
+var age = 17;
+var homme=true;
+
+console.log("nom : "+" maria");
+console.log("age : "+17);
+if (homme=true)
+    console.log("homme")
+else
+    console.log("femme")
+
+
